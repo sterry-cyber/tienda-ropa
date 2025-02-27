3f04ef7560ba528619e242df959f1c5b1fe6f1d9
sterry-cyber <dwaynewilson025@gmail.com>
2025-02-27 15:33:23 -0600

    first commit

---
+++ Header.php
@@ -0,0 +1,15 @@
+<div id="title">
+    <h1><span style="color: #008000;">Venta en Linea</span></h1>
+  </div>
+  
+<div class="container">
+    <ul id="navCircle">
+      <li><a class="active" href="index.php">Inicio</a></li>
+
+      <li><a href="Category.php">Categoria</a></li>
+      <li><a href="Products.php">Productos</a></li>
+      <li><a href="Offers.php">Ofertas</a></li>
+ <li><a href="Register.php">Registro</a></li>
+      <li><a href="Contact.php">Contacto</a></li>
+    </ul>
+  </div>
